ce87131e5bcee6171f1809741150ddba2c14df6e
KhalilE792 <khalilecollege@gmail.com>
2025-10-01 21:14:45 -0500

    bugfix5

--- src/basics.ml
+++ src/basics.ml
@@ -7,9 +7,26 @@ let abs x =
 (***********************************)
 
 let rev_tup (tup : 'a * 'b) = (snd tup, fst tup);;
+
+(*helper functions*)
+let fst3 (tup: 'a * 'b * 'c) = 
+  match tup with
+    (a, b, c) -> a
+;;
+
+let snd3 (tup: 'a * 'b * 'c) = 
+  match tup with
+    (a, b, c) -> b
+;;
+
+let thd3 (tup: 'a * 'b * 'c) = 
+  match tup with
+    (a, b, c) -> c
+;;
+
 let rev_triple (tup : 'a * 'b * 'c) = (thd3 tup, snd3 tup, fst3 tup);;
 
-let is_odd x = if (x % 2) = 1 then true else false;;
+let is_odd x = if x mod 2 = 1 then true else false;;
 
 let is_older (date1: int * int * int) (date2: int * int * int) = 
   if fst3 date1 < fst3 date2 then true else
@@ -32,10 +49,10 @@ let rec fac n = if n = 1 then 1 else (n * fac (n - 1));;
 (* Part 3: Lists *)
 (*****************)
 
-let rec get_nth ((idx:int), (lst: 'a list)) = if idx = 0 then List.hd (lst) else get_nth (idx - 1) List.tl (lst);;
+let rec get_nth ((idx:int), (lst: 'a list)) = if idx = 0 then List.hd (lst) else get_nth ((idx - 1), List.tl (lst));;
 
 (*helper function*)
-let rec list_length (lst: 'a list) = if lst = [] then 0 else (1 + list.length (List.tl (lst)));;
+let rec list_length (lst: 'a list) = if lst = [] then 0 else (1 + list_length (List.tl (lst)));;
 
 let larger lst1 lst2 = 
   if list_length (lst1) = list_length (lst2) then [] else
